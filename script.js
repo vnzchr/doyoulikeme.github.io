02252e3adf76a11efcca2a3779c8ec0f6c6c7750
vnzchr <152945651+vnzchr@users.noreply.github.com>
2026-04-16 20:53:40 +0700

    Update script.js

--- script.js
+++ script.js
@@ -21,3 +21,22 @@ noBtn.addEventListener("mouseover", () => {
   noBtn.style.left = randomX + "px";
   noBtn.style.top = randomY + "px";
 });
+
+const bgm = document.getElementById("bgm");
+const btn = document.getElementById("musicBtn");
+const icon = document.getElementById("icon");
+const label = document.getElementById("label");
+
+btn.addEventListener("click", () => {
+  if (bgm.paused) {
+    bgm.play();
+    btn.classList.add("playing");
+    icon.textContent = "⏸";
+    label.textContent = "Pause Music";
+  } else {
+    bgm.pause();
+    btn.classList.remove("playing");
+    icon.textContent = "▶";
+    label.textContent = "Play Music";
+  }
+});
